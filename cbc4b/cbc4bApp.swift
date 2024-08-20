6b1002e2ba12b563e876e69c17719ec0fffde58f
jasmimi <jasmine_amohia@hotmail.com>
2024-08-20 20:56:29 +1200

    Initial Commit

---
+++ cbc4b/cbc4bApp.swift
@@ -0,0 +1,17 @@
+//
+//  cbc4bApp.swift
+//  cbc4b
+//
+//  Created by Jasmine Amohia on 20/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct cbc4bApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
