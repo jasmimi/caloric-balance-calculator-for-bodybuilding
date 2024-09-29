7af8bdc1ca93274b41170203bd24399765c69d0e
jasmimi <jasmine_amohia@hotmail.com>
2024-09-29 21:30:16 +1300

    Share AppleHealthKit data for caloric intake and expenditure

--- cbc4b/Views/Setup/ShareData.swift
+++ cbc4b/Views/Setup/ShareData.swift
@@ -6,13 +6,35 @@
 //
 
 import SwiftUI
+import HealthKit
 
 struct ShareData: View {
     @EnvironmentObject var authManager: AuthManager
     @State private var appleHealthToggle = false
-    @State private var myFitnessPalToggle = false
     @State private var showAlert = false
     var atitle: String
+    
+    let healthStore = HKHealthStore()
+    
+    // Function to request permission for HealthKit data
+    private func requestHealthKitAccess() {
+        // Safely unwrap the HKObjectTypes
+        if let activeEnergyBurned = HKObjectType.quantityType(forIdentifier: .activeEnergyBurned),
+           let dietaryEnergyConsumed = HKObjectType.quantityType(forIdentifier: .dietaryEnergyConsumed) {
+            
+            let allTypes: Set<HKObjectType> = [activeEnergyBurned,
+                                               HKObjectType.workoutType(),
+                                               dietaryEnergyConsumed]
+            
+            healthStore.requestAuthorization(toShare: [], read: allTypes) { success, error in
+                if !success {
+                    print("HealthKit authorization failed: \(String(describing: error?.localizedDescription))")
+                }
+            }
+        } else {
+            print("Failed to unwrap HKObjectTypes.")
+        }
+    }
 
     var body: some View {
         NavigationStack {
@@ -23,23 +45,25 @@ struct ShareData: View {
                 Toggle(isOn: $appleHealthToggle) {
                     Text("Apple Health data")
                 }
-                Toggle(isOn: $myFitnessPalToggle) {
-                    Text("MyFitnessPal data")
+                .onChange(of: appleHealthToggle) { value in
+                    if value {
+                        requestHealthKitAccess()
+                    }
                 }
                 
                 NavigationLink("Continue") {
                      ContentView()
                          .onAppear {
-                             if appleHealthToggle && myFitnessPalToggle {
+                             if appleHealthToggle {
                                  authManager.markShareDataComplete()
                              }
                          }
                  }
-                .disabled(!(appleHealthToggle && myFitnessPalToggle))
+                .disabled(!(appleHealthToggle))
                 .padding()
                 
-                if !(appleHealthToggle && myFitnessPalToggle) {
-                    Text("Please enable both toggles to continue")
+                if !(appleHealthToggle) {
+                    Text("Please enable toggle to continue")
                         .foregroundColor(.red)
                 }
             }
@@ -47,7 +71,6 @@ struct ShareData: View {
         }
     }
 }
-
 
 #Preview {
     ShareData(atitle: "Tc2nvjU9gDgbCyF5eNBhGMDnFG72")
